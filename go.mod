79620a3d6a05ebfc4b3db313fa12294e46095701
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-08 17:53:32 +0100

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dominikus1993/golang-playground
 
 go 1.20
+
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"image/jpeg"
+	"image/png"
+	"os"
+
+	"github.com/nfnt/resize"
+)
 
 func main() {
-	fmt.Println("test")
+	f, err := os.Open("./jp2137.jpg")
+
+	if err != nil {
+		panic(err)
+	}
+	output, _ := os.Create("jp2137_resized.png")
+	defer output.Close()
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Bytes()
+	image, err := jpeg.Decode(f)
+	// check err
+
+	if err != nil {
+		panic(err)
+	}
+	newImage := resize.Resize(160, 0, image, resize.Lanczos3)
+
+	// Encode uses a Writer, use a Buffer if you need the raw []byte
+	err = png.Encode(output, newImage)
+	// check err
+	if err != nil {
+		panic(err)
+	}
 }
